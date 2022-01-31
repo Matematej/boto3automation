b2a224e6684358eb362aef642d2dd0083681b9bd
Matej <sterba.matej@icloud.com>
2022-01-31 18:16:49 +0100

    first commit

---
+++ EC2_start_stop/EC2_lambda/start_ec2.py
@@ -0,0 +1,16 @@
+import json
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+ec2_filter={'Name': 'teg:test_id','Values': ['start-stop']}
+def lambda_handler(event, context):
+    for each in ec2.instances.filter(Filters=[ec2_filter]):
+        print(each)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world"
+        }),
+    }
