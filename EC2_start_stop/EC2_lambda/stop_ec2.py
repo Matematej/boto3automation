37e764f6fe18e255b37bf9d818d38977a2403317
Matej <sterba.matej@icloud.com>
2022-02-03 19:45:17 +0100

    fourth commit

--- EC2_start_stop/EC2_lambda/stop_ec2.py
+++ EC2_start_stop/EC2_lambda/stop_ec2.py
@@ -3,8 +3,8 @@ import boto3
 
 ec2 = boto3.resource('ec2')
 
-ec2_filter={'Name': 'teg:test_id','Values': ['start-stop']}
 def lambda_handler(event, context):
+    ec2_filter={"Name": "tag:test_id","Values": ["start-stop"]}
     for each in ec2.instances.filter(Filters=[ec2_filter]):
         each.stop()
 
