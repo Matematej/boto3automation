98edcd78b6ac3618ea44b31321a0455021d36070
Matej <sterba.matej@icloud.com>
2022-02-04 02:13:35 +0100

    fifth commit

--- EC2_start_stop/EC2_lambda/stop_ec2.py
+++ EC2_start_stop/EC2_lambda/stop_ec2.py
@@ -1,16 +1,26 @@
 import json
 import boto3
+import os
 
-ec2 = boto3.resource('ec2')
-
+Ec2 = boto3.resource('ec2')
+client = boto3.client('ec2')
+sns_client = boto3.client('sns')
+waiter = client.get_waiter('instance_stopped')
+Ids_EC2 = []
+        
 def lambda_handler(event, context):
     ec2_filter={"Name": "tag:test_id","Values": ["start-stop"]}
-    for each in ec2.instances.filter(Filters=[ec2_filter]):
+    for each in Ec2.instances.filter(Filters=[ec2_filter]):
         each.stop()
-
+        Ids_EC2.append(each.instance_id)
+        waiter.wait(InstanceIds = Ids_EC2)
+        
+    if Ids_EC2:
+        snsArn = os.environ["SNSARN"]
+        sns_client.publish(TopicArn=snsArn, Message='EC2 had been stopped', Subject='success')
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world"
+            "message": "EC2 Stopped"
         }),
     }
